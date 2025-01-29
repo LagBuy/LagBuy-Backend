c734abe83b709c9d045ebe1d9da5a3c95b87830e
Chisom Daniel <chinwezechisom@gmail.com>
2025-01-29 14:39:25 +0100

    fixing aws ebs config files 4

--- lagbuy/settings.py
+++ lagbuy/settings.py
@@ -12,6 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = env.str("SECRET_KEY")
 
+if not SECRET_KEY:
+    raise ValueError("Missing DJANGO_SECRET_KEY environment variable")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
