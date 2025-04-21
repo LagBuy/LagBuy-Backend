4662676d42c7a55bf24a25aae413d26d50c19531
chisomdaniel <chinwezechisom@gmail.com>
2025-04-21 16:26:43 +0100

    feat: Added product search and product filter feature - Added functionality to search for product using any string to the product list view - The search string will search through the following fields on the product model: name, description and categories name - Added product filter feature to filter products by: ,  and - Added more TODOs to be handled on the product serializer class
    '

---
+++ apps/products/filter.py
@@ -0,0 +1,11 @@
+from django_filters import rest_framework as filters
+from .models import Product
+
+class ProductFilter(filters.FilterSet):
+    """A filter set class for the product view"""
+    min_price = filters.NumberFilter(field_name="price", lookup_expr="gte")
+    max_price = filters.NumberFilter(field_name="price", lookup_expr="lte")
+
+    class Meta:
+        model = Product
+        fields = ["categories", "verified"]
