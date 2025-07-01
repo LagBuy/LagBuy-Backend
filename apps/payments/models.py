232c3fc1b8239046a38b4fc60bac09a0e2737c85
Truelife Agada <abramsharry6@gmail.com>
2025-07-01 22:28:48 +0100

    feat: Implement payment processing feature with transaction initialization, verification, and refund capabilities

--- apps/payments/models.py
+++ apps/payments/models.py
@@ -4,46 +4,39 @@ import uuid
 from django.db import models
 from django.utils import timezone
 
-from apps.userAuth.models import CustomUser
 from apps.orders.models import Order
+from apps.userAuth.models import CustomUser
 
 
 class Payment(models.Model):
     """Model to store and track all platform payment"""
 
-    id = models.UUIDField(
-        primary_key=True, default=uuid.uuid4, editable=False
-    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     amount = models.DecimalField(max_digits=15, decimal_places=2)
-    ref = models.UUIDField(unique=True)
+    ref = models.CharField(
+        max_length=100, unique=True, help_text="Unique reference for the payment"
+    )
     verified = models.BooleanField(default=False)
     currency = models.CharField(max_length=5)
     created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
     # Relationships
     user = models.ForeignKey(
-        CustomUser,
-        on_delete=models.SET_NULL,
-        null=True,
-        related_name="payments"
+        CustomUser, on_delete=models.SET_NULL, null=True, related_name="payments"
     )
     order = models.OneToOneField(
-        Order,
-        on_delete=models.SET_NULL,
-        null=True,
-        related_name='payment'
+        Order, on_delete=models.SET_NULL, null=True, related_name="payment"
     )
 
     class Meta:
         verbose_name = "Payment"
         verbose_name_plural = "Payments"
         ordering = ["created_at"]
-    
+
     def __str__(self):
         return f"Payment of N{self.amount} made by {self.user.user_profile.first_name}"
 
     def verify_payment(self):
         """verify the payment using paystack verify endpoint"""
         pass
-
--- lagbuy/urls.py
+++ lagbuy/urls.py
@@ -8,20 +8,23 @@ from .views import APIStatusView
 from dj_rest_auth.views import PasswordResetView, PasswordResetConfirmView
 
 urlpatterns = [
-    path("", APIStatusView.as_view(), name='api-status'),
+    path("", APIStatusView.as_view(), name="api-status"),
     path("admin/", admin.site.urls),
     path("api/v1/auth/", include("apps.userAuth.urls"), name="user_authentication"),
     path("api/v1/products/", include("apps.products.urls")),
     path("api/v1/orders/", include("apps.orders.urls"), name="orders"),
+    path("api/v1/payments/", include("apps.payments.urls"), name="payments"),
     path("api/v1/coupon/", include("apps.coupons.urls"), name="coupon"),
     path("api/v1/cart/", include("apps.cart.urls"), name="cart"),
     path("api/v1/reviews/", include("apps.reviews.urls"), name="reviews"),
     path("api/v1/vendors/", include("apps.vendors.urls"), name="analytic"),
 
     # password reset
-    path("password/reset/<uidb64>/<token>/",
-          PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-
+    path(
+        "password/reset/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
     # schema
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
