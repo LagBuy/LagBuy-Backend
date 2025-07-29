399b65abfb03b3c2546da06cb1fcd276ad5162dd
chisomdaniel <chinwezechisom@gmail.com>
2025-07-29 14:07:48 +0100

    quick fix

--- apps/riders/urls.py
+++ apps/riders/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path("items-assign/", AdminListAllOrder.as_view(), name="admin-list-all-items"),
     path("items/accepted/", GetUndeliveredOrderItems.as_view(), name="get-all-undelivered-items"),
     path("items/assigned/", GetAllAssignedOrderRequest.as_view(), name="get-all-assigned-order"),
-    path("items/accept/<uuid:item_id>", AcceptOrDeclineOrderRequest.as_view(), name="accept-or-decline-assigned-order"),
+    path("items/accept/<uuid:item_id>/", AcceptOrDeclineOrderRequest.as_view(), name="accept-or-decline-assigned-order"),
     path("items/delivered/", GetDeliveredOrderItems.as_view(), name="get-all-delivered-items")
 ]
