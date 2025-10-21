cd96236f042d64b57cc3c4be4ec6c3d0daaad0bb
chisomdaniel <chinwezechisom@gmail.com>
2025-10-21 13:59:23 +0100

    fix(settings): change DEBUG default from True to False for production safety

--- lagbuy/settings.py
+++ lagbuy/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=True)
+DEBUG = env.bool("DEBUG", default=False)
 
 APPEND_SLASH = True  #
 MEDIA_URL = "/media/"  #
